78bb52b0ad32d5ad992a7c2cd00f852a06a99368
unknown <601732644@qq.com>
2024-11-18 10:49:48 +0800

    chore(build_file):编译脚本调整

--- script/env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cmake_dir="/c/Qt/Tools/CMake_64/bin"
-ninja_dir="/c/Qt/Tools/Ninja"
-mingwLib_dir="/c/Qt/6.6.3/mingw_64/bin"
-mingw_dir="/c/Qt/Tools/mingw1120_64/bin"
-echo current is env.sh
